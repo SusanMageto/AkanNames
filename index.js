46b227d04068e2f02a1c4c52d39a0486331e89f4
SusanMageto <susan.mageto@student.moringaschool.com>
2022-03-06 21:19:50 +0300

    adds else if statements so as to get the names of the other days

--- index.js
+++ index.js
@@ -4,9 +4,27 @@ function myFunction() {
   if (gender==="female") {
     var d = new Date(date);
     var s = d.getDay();
-    
+
     if (s==0){
       alert ("Akosua");
+    }
+    else if (s==1) {
+      alert ("Adwoa");
+    }
+    else if (s==2) {
+      alert ("Abenaa");
+    }
+    else if (s==3) {
+      alert ("Akua");
+    }
+    else if (s==4) {
+      alert ("Yaa");
+    }
+    else if (s==5) {
+      alert ("Afua");
+    }
+    else if (s==6) {
+      alert ("Ama");
     }
   }
 }
