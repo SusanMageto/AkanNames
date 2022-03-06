927ed2ed05d7d6ca3563b62620a7dbfa2e3e53a1
SusanMageto <susan.mageto@student.moringaschool.com>
2022-03-06 21:14:37 +0300

    declares the new date and getDay variables

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function myFunction() {
   var date = document.getElementById ("date").value;
   var gender = document.getElementById ("gender").value;
   if (gender==="female") {
-    
+    var d = new Date(date);
+    var s = d.getDay();
   }
 }
