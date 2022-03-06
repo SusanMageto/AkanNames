1736671c04b8bfbd695fb6bf864fff0dbfe8c7b7
SusanMageto <susan.mageto@student.moringaschool.com>
2022-03-06 21:23:44 +0300

    adds an if and else if statements for the male names

--- index.js
+++ index.js
@@ -30,5 +30,26 @@ function myFunction() {
   else {
     var d = new Date(date);
     var s = d.getDay();
+    if (s==0){
+      alert ("Kwasi");
+    }
+    else if (s==1) {
+      alert ("Kwadwo");
+    }
+    else if (s==2) {
+      alert ("Kwabena");
+    }
+    else if (s==3) {
+      alert ("Kwaku");
+    }
+    else if (s==4) {
+      alert ("Yaw");
+    }
+    else if (s==5) {
+      alert ("Kofi");
+    }
+    else if (s==6) {
+      alert ("Kwame");
+    }
   }
 }
